979249c2d332e0a9233770626af89980c5c4cbf0
Yusuf Pisan <pisan@uw.edu>
2022-09-28 09:38:08 -0700

    partial updates

---
+++ students-partial/combine.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat header.html > ../students.html
+
+cat 2022AU.html >> ../students.html
+cat 2022SP.html >> ../students.html
+cat 2022WI.html >> ../students.html
+
+cat 2021.html >> ../students.html
+
+cat footer.html >> ../students.html
